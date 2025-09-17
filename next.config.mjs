45714a4ec1624823e399fe7cf5c866db59537a7d
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-09-18 00:21:54 +0600

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    devIndicators: {
+    buildActivity: false,   // Removes the “N” logo spinner
+  },
+    
+};
 
 export default nextConfig;
---
+++ src/app/State.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function State() {
+  return (
+    <div>
+      <h1>This is Sate from Sate.jsx</h1>
+    </div>
+  )
+}
+
+export default State
